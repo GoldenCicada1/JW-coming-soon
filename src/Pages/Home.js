ec3f8f0559aac210405129f88ed9bc25e51b97d8
Goodluck Japhet <goodluckjaphet7@gmail.com>
2024-09-23 15:39:18 +0300

    Refactor package.json and manifest.json
    - Updated the name field in package.json and manifest.json from "countdowm" to "jw".
    - Made changes to the font and layout in index.css.
    - Removed the logo.svg file from the assets/images folder.
    - Updated the logo image in Home.js from logo.svg to logo.png.
    - Updated the banner text and added a timer in Home.js.
    - Updated the text and layout in Timer.js.
    - Added input and button styling in index.css.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,59 +1,62 @@
-import React, { Component } from "react";
-import Logo from "../assets/images/logo.svg";
+import React from "react";
+import Logo from "../logo.png";
 import Banner from "../assets/images/banner.svg";
 import Timer from "./components/Timer";
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div id="home">
-        <section>
-          <nav>
-            <div className="container">
-              <img src={Logo} alt="Company Logo" className="brand" />
-            </div>
-          </nav>
-          <div className="cid-rOM5rKjpOn">
-            <div className="container">
-              <div className="row justify-content-center">
-                <div className="col-lg-6 col-md-6 col-sm-12 text-section">
-                  <h1 className="display-1">Coming Soon!</h1>
-                  <p className="banner-text display-7">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s.
-                  </p>
-                  <div className="form-container">
-                    <form>
-                      <div className="form-row align-items-center">
-                        <div className="col-auto">
-                          <div className="input-group mb-2">
-                            <input
-                              type="email"
-                              className="form-control"
-                              placeholder="Enter Your Email Address"
-                              aria-label="Enter Your Email Address"
-                            />
-                            <div className="input-group-append">
-                              <button className="btn btn-primary">Notify Me</button>
-                            </div>
+const Home = () => {
+  return (
+    <div id="home">
+      <section>
+        <nav>
+          <div className="container">
+            <img src={Logo} alt="logo" className="brand" />
+          </div>
+        </nav>
+        <div className="cid-rOM5rKjpOn">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-6 col-md-6 col-sm-12 text-section">
+                <h1 className="display-1">Coming soon!</h1>
+                <p className="banner-text display-7">
+                  At Japhets Wings, we’re dedicated to elevating your
+                  experience with innovative solutions and exceptional service.
+                  Stay tuned as we prepare to take flight and unveil something
+                  truly special. Your journey begins soon!{" "}
+                </p>
+                <div className="form-container">
+                  <form>
+                    <div className="form-row align-items-center">
+                      <div className="col-auto">
+                        <div className="input-group mb-2">
+                          <input
+                            type="email"
+                            className="form-control"
+                            placeholder="Enter Your Email Address"
+                            aria-label="Enter Your Email Address"
+                          />
+                          <div className="input-group-append">
+                            <button className="btn btn-primary">
+                              Notify Me
+                            </button>
                           </div>
                         </div>
                       </div>
-                    </form>
-                  </div>
+                    </div>
+                  </form>
                 </div>
-                <div className="col-lg-6 col-md-6 col-sm-12 banner-container">
-                  <img src={Banner} alt="Coming Soon Banner" className="banner" />
-                  <div className="my-3">
-                    <Timer />
-                  </div>
+              </div>
+              <div className="col-lg-6 col-md-6 col-sm-12 banner-container">
+                <img src={Banner} alt="Banner" className="banner" />
+                <div className="my-3 timer">
+                  <Timer />
                 </div>
               </div>
             </div>
           </div>
-        </section>
-      </div>
-    );
-  }
-}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
--- src/Pages/components/Timer.js
+++ src/Pages/components/Timer.js
@@ -1,85 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 export default function Timer() {
-  const calculateTimeLeft = () => {
-    //let year = new Date().getFullYear();
-    let nextday = new Date(
-      new Date().getFullYear(),
-      new Date().getMonth(),
-      new Date().getDate() + 3
-    );
-    const difference = +nextday - +new Date();
-    let timeLeft = {};
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      };
+  const calculateTimeLeft = useCallback(() => {
+    const today = new Date();
+    const nextday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+    const difference = nextday - today;
+
+    if (difference <= 0) {
+      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
     }
-    return timeLeft;
-  };
+
+    return {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / (1000 / 60)) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }, []);
+
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const timer = setInterval(() => {
       setTimeLeft(calculateTimeLeft());
     }, 1000);
-    return () => clearTimeout(timer);
-  });
+    return () => clearInterval(timer);
+  }, [calculateTimeLeft]);
 
-  const timerComponents = [];
+  const { days = 0, hours = 0, minutes = 0, seconds = 0 } = timeLeft;
 
-  Object.keys(timeLeft).forEach((interval) => {
-    if (!timeLeft[interval]) {
-      return;
-    }
-
-    timerComponents.push(
-      <span>
-        {timeLeft[interval]} {interval}{" "}
-      </span>
-    );
-  });
+  const timeUnits = [
+    { label: "DAYS", value: days },
+    { label: "HOURS", value: hours },
+    { label: "MINUTES", value: minutes },
+    { label: "SECONDS", value: seconds },
+  ];
 
   return (
-    <div className="timer">
-      <div className="d-flex justify-content-between">
-        <div className="text-center">
-          <p className="">{timeLeft.days}</p>
-          <h5>DAYS</h5>
-        </div>
-        <div className="sep">:</div>
-        <div className="text-center">
-          <p className="">{timeLeft.hours}</p>
-          <h5>HOURS</h5>
-        </div>
-        <div className="sep">:</div>
-        <div className="text-center">
-          <p>{timeLeft.minutes}</p>
-          <h5>MINUTES</h5>
-        </div>
-        <div className="sep">:</div>
-        <div className="text-center">
-          <p>{timeLeft.seconds}</p>
-          <h5>SECONDS</h5>
-        </div>
-      </div>
-{/*       <div className="d-flex justify-content-between align-items-center">
-        <div>
-          <h5 className="text-center">DAYS</h5>
-        </div>
-        <div>
-          <h5 className="text-center">HOURS</h5>
-        </div>
-        <div className="text-center">
-          <h5>MINUTES</h5>
-        </div>
-        <div className="text-center">
-          <h5>SECONDS</h5>
-        </div>
-      </div> */}
+    <div className="timer d-flex justify-content-between">
+      {timeUnits.map((unit, index) => (
+        <React.Fragment key={unit.label}>
+          <div className="text-center">
+            <p>{unit.value}</p>
+            <h5>{unit.label}</h5>
+          </div>
+          {index < timeUnits.length - 1 && <div className="sep">:</div>}
+        </React.Fragment>
+      ))}
     </div>
   );
 }
